4a2b91020f2b1d3e8ebc6978c66ecfb126519d25
elissandroa <elissandroa@hotmail.com>
2025-06-24 19:00:19 -0300

    Ajuste nos comentários

--- ContaBanco/src/Main/App.java
+++ ContaBanco/src/Main/App.java
@@ -13,20 +13,19 @@ public class App {
         System.out.println("Bem-vindo ao Santander!");
         System.out.print("Digite o número da conta: ");
         int numeroConta = sc.nextInt();
-        sc.nextLine(); // Consumir a quebra de linha pendente
+        sc.nextLine();
         System.out.print("Digite o nome do cliente: ");
         String nomeCliente = sc.nextLine();
         System.out.print("Digite o número da agência: ");
         String agencia = sc.nextLine();
 
-        // Criar a conta com os dados fornecidos
         ContaTerminal conta = new ContaTerminal(numeroConta, agencia, nomeCliente);
-        // Solicitar o valor do depósito inicial
+
         System.out.print("Digite o valor do depósito inicial: ");
         double valorDeposito = sc.nextDouble();
-        // Depositar o valor na conta
+
         conta.depositar(valorDeposito);
-        // Exibir os dados da conta
+
         System.out.println("Olá " + conta.getNomeCliente()
                 + ", obrigado por criar uma conta em nosso banco!, sua agência é "
                 + conta.getAgencia() + ", conta " + conta.getNumero() + " e seu saldo já está disponível para saque");
